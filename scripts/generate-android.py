08c8fb52687b29851e12155c5fa6eb03da62953e
ZavaruKitsu <alexeyzavar@gmail.com>
2023-06-25 19:10:50 +0300

    feat: new strings fix: Android generation

--- scripts/generate-android.py
+++ scripts/generate-android.py
@@ -1,3 +1,5 @@
+import re
+
 from lxml import etree as et
 
 import json
@@ -28,8 +30,14 @@ for lang in langs:
     root = et.Element('resources')
 
     for k, v in strings.items():
+        if k + '_Android' in strings:
+            continue
+
+        if k.endswith('_Android'):
+            k = k.replace('_Android', '')
+
         string = et.SubElement(root, 'string', {'name': k})
-        string.text = v
+        string.text = v.replace('\'', '\\\'')
 
     tree = et.ElementTree(root)
 
@@ -38,7 +46,15 @@ for lang in langs:
     if not os.path.exists(f'./out/android/values{suffix}'):
         os.mkdir(f'./out/android/values{suffix}')
 
-    tree.write(f'./out/android/values{suffix}/ayu.xml', pretty_print=True, xml_declaration=True, encoding="utf-8")
+    tree.write(f'./out/android/values{suffix}/ayu.xml', pretty_print=True, xml_declaration=True, encoding='utf-8')
+
+    # fix CDATA
+    with open(f'./out/android/values{suffix}/ayu.xml', encoding='utf-8') as f:
+        data = f.read()
+        data = re.sub(r'&lt;!\[CDATA\[&lt;a href="(.+?)"&gt;(.+?)&lt;/a&gt;\]\]&gt;', r'<![CDATA[<a href="\1">\2</a>]]>', data)
+
+    with open(f'./out/android/values{suffix}/ayu.xml', 'w', encoding='utf-8') as f:
+        f.write(data)
 
     print(f'Processed "{lang}"')
 
