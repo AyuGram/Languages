7615a5c009e04e16dd3b9893c84e311c996be7a9
ZavaruKitsu <alexeyzavar@gmail.com>
2023-08-09 20:22:58 +0000

    feat: new strings fix: truncate `_Desktop`

--- scripts/generate-desktop.py
+++ scripts/generate-desktop.py
@@ -24,6 +24,9 @@ for k, v in strings.items():
     if k.endswith('_Android'):
         continue
 
+    if k.endswith('_Desktop'):
+        k = k.replace('_Desktop', '')
+
     escaped = v.replace('"', '\\"').replace('\n', '\\n')
     data += f'"ayu_{k}" = "{escaped}";\n'
 
