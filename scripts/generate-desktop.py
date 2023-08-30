cec5b552913b6ce5b899afa4857747a576c64c86
ZavaruKitsu <alexeyzavar@gmail.com>
2023-08-30 20:06:45 +0000

    feat: new strings fix: take strings from the latest tag

--- scripts/generate-desktop.py
+++ scripts/generate-desktop.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+import re
 
 import requests
 
@@ -9,8 +10,19 @@ if not os.path.exists('./values'):
 with open('./values/langs/en/Shared.json', encoding='utf-8') as f:
     strings = json.load(f)
 
+
+def parse_latest_tag():
+    r = requests.get('https://github.com/telegramdesktop/tdesktop/tags')
+    regex = re.compile(r'<a href="/telegramdesktop/tdesktop/releases/tag/(.*?)"')
+
+    try:
+        return regex.search(r.text).group(1)
+    except:
+        return None
+
+
 req = requests.get(
-    'https://raw.githubusercontent.com/telegramdesktop/tdesktop/dev/Telegram/Resources/langs/lang.strings'
+    f'https://raw.githubusercontent.com/telegramdesktop/tdesktop/{parse_latest_tag()}/Telegram/Resources/langs/lang.strings'
 )
 
 data = req.text
